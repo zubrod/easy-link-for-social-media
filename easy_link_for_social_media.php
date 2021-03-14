47a0172f9f9b9af4b336f32276ebc93b2df8cf82
P_Zubrod <supertex1337@freenet.de>
2021-03-14 16:02:18 +0100

    Add essential plugin information

---
+++ easy_link_for_social_media.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Easy Link For Social Media
+ * Plugin URI: https://www.coloredsurge.com
+ * Description: This is a simple way to group your links for your social media pages.
+ * Author: Paul Zubrod
+ * Author URI: https://www.coloredsurge.com
+ * Version: 1.0.0
+ * License GPL2
+ * 
+ */
+
+defined( 'ABSPATH' ) or die( 'Nope, not accessing this' );
